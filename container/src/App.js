21b290d7fe86236f4988de6a1ce93a459e608434
Abhishek Kumar <Abhishek.kumar4@walmart.com>
2022-10-21 11:17:36 +0530

    fix: css colision in the app

--- container/src/App.js
+++ container/src/App.js
@@ -3,14 +3,24 @@ import { BrowserRouter } from "react-router-dom";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles/";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
